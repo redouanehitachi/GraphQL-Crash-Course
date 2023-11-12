a1f4dacb706e399a38dd9bc6dfd8faf28daa0cc5
redouane <redouane.hitachi@gmail.com>
2023-11-11 17:02:51 -0800

    Resolver Functions

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-import ApolloServer from '@apollo/server'
-import startStandaloneServer from '@apollo/server/standalone'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import db from './_db.js'
+
 // types
-import { typeDefs } from './schema'
+
+import { typeDefs } from './schema.js'
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+}
 // server setup
 
 
 const server = new ApolloServer({
     typeDefs,
 
-    // resolvers
+    resolvers
+
 
 })
 const { url } = await startStandaloneServer(server, {
